32a07436297fa43909594069e0fabf48b4e59e27
Sangram Mohanty <odisha532@hotmail.com>
2021-04-26 11:30:58 +0530

    Transpose of a matrix.

--- 11_Matrix.py
+++ 11_Matrix.py
@@ -4,3 +4,7 @@ _mat = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
 print(_mat[0])
 print(_mat[1])
 print(_mat[2])
+
+# Transpose of a matrix covered in list comprehension
+_mat = [[_mat[inc][j] for inc in [i for i in range(len(_mat))]] for j in range(4)]
+print(_mat)
