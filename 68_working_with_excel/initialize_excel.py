60c9c6ad6d2179aa670583e4e72f40f51465079b
Sangram Mohanty <odisha532@hotmail.com>
2021-05-12 11:54:05 +0530

    Added iter_rows & iter_cols methods to select a range.

--- 68_working_with_excel/initialize_excel.py
+++ 68_working_with_excel/initialize_excel.py
@@ -4,6 +4,20 @@ ex = openpyxl.load_workbook('collegeDetails.xlsx')
 colnum = 65
 cell = chr(colnum)
 rownum = 2
+
+# to retrieve headers
+headers = [val[0].value for val in [row for row in ex['student'].columns]]
+print(headers)
+
+# to select a cell range row wise
+cellrange = list(ex['student'].iter_rows(2,ex['student'].max_row,1,2))
+for row in cellrange:
+    print(row[0].value,row[1].value)
+# to select a cell range column wise
+cellrange = list(ex['student'].iter_cols(1,2,2,ex['student'].max_row))
+for col in cellrange:
+    print(col[0].value,col[1].value,col[2].value,col[3].value,col[4].value,col[5].value)
+
 while (ex['student'][cell + str(rownum)].value != None):
     print(ex['student'][cell + str(rownum)].value, end=' ')
     colnum += 1
