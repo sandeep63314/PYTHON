224b9b2b18b9ad9b1a81cc65e518aac7eb87a335
Sangram Mohanty <odisha532@hotmail.com>
2021-09-26 16:55:13 +0530

    to find octal and hexadecimal value of a number.

--- 2_complex_bin.py
+++ 2_complex_bin.py
@@ -5,6 +5,17 @@ print(c)
 # to find binary value of a number
 d = bin(100)
 print(d)
+# to find octal value of a number
+o = oct(200)
+print(o)
+# to find hexadecimal value of a number and it's type
+h = hex(300)
+print(h,type(h))
 # to find integer value of a binary number
 d = int('0b1100100', 2)
 print(d)
+# to find integer value of a octal number
+o = int('0o310',8)
+print(o)
+h = int('0x12c',16)
+print(h)
