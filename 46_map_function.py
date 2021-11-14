e4e5d47b4c8b171963b84c72be6fb74941c98966
Sangram Mohanty <odisha532@hotmail.com>
2021-11-14 10:32:53 +0530

    Used map function to convert type of elements inside a collection

--- 46_map_function.py
+++ 46_map_function.py
@@ -2,6 +2,7 @@
 # Map function is used to work with each element of iterables passed as argument in it and returns a iterable list,tuple or set.
 
 # Though 'inc' is not iterable but I have passed it as a default parameter
+import datetime
 
 def salary_increment(salary, inc=10):
     return ((100 + inc) / 100) * salary
@@ -11,3 +12,10 @@ _sal = [25000, 30000, 40000]
 _salaries = tuple(map(salary_increment, _sal))
 
 print(_salaries)
+
+_dob = input('Enter your date of birth in dd-mm-yyyy format:')
+_dob = _dob.split('-')
+_dob = tuple(map(int,_dob))
+_dd, _mm, _yyyy = _dob
+print(type(_dd),type(_mm),type(_yyyy))
+print('You are eligible to receive driving licence' if (int(datetime.date.today().strftime('%Y')) - _yyyy) > 18 else 'You are not eligible to drive')
