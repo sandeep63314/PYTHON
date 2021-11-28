bef9bfc6343b1619d7a87e5682278325a44caffd
Sangram Mohanty <odisha532@hotmail.com>
2021-11-28 09:42:22 +0530

    Modified contents and written it into a csv file.

--- 67_working_with_csv/4_custom_dictionarywriter_csv.py
+++ 67_working_with_csv/4_custom_dictionarywriter_csv.py
@@ -5,9 +5,33 @@ content = [{'itemid':100,'itemname':'rice','price':45},
            {'itemid':102,'itemname':'soyabean','price':80}
            ]
 
-fieldnames = ['itemid','itemname','price','discount','quantity_unit']
+# fieldnames = ['itemid','itemname','price','discount','quantity_unit']
+#
+# with open('custom_dict_write.csv','w',newline='') as write_csv:
+#     csv_writer = csv.DictWriter(write_csv,fieldnames = fieldnames)
+#     csv_writer.writeheader()
+#     csv_writer.writerows(content)
+
+for c in content:
+    if c['itemid'] == 100:
+        c['quantity_unit'] = 2
+        c['discount'] = '5%'
+        c['totalprice'] = ((100 - eval(c['discount'].rstrip('%')))/100) * c['quantity_unit'] * c['price']
+    if c['itemid'] == 101:
+        c['quantity_unit'] = 3
+        c['discount'] = '3%'
+        c['totalprice'] = ((100 - eval(c['discount'].rstrip('%')))/100) * c['quantity_unit'] * c['price']
+    if c['itemid'] == 102:
+        c['quantity_unit'] = 5
+        c['discount'] = '7%'
+        c['totalprice'] = ((100 - eval(c['discount'].rstrip('%')))/100) * c['quantity_unit'] * c['price']
+
+print(content)
+
+fieldnames = ['itemid','itemname','price','quantity_unit','discount','totalprice']
 
 with open('custom_dict_write.csv','w',newline='') as write_csv:
     csv_writer = csv.DictWriter(write_csv,fieldnames = fieldnames)
     csv_writer.writeheader()
     csv_writer.writerows(content)
+
--- 67_working_with_csv/5_working_with_pandas.py
+++ 67_working_with_csv/5_working_with_pandas.py
@@ -2,4 +2,7 @@ import pandas
 
 read_csv = pandas.read_csv('grocery.csv')
 
-print(read_csv)
+print(read_csv) # print contents of a csv file
+print(f'Total number of rows in grocery.csv file is {len(read_csv)}') # print total number of rows in a csv file
+print(read_csv['date_of_purchase'].loc[0:3]) # print rows of a particular file. You can specify start and end index values of rows
+print(dict(read_csv.loc[1:3])) # print rows from start and end index values of rows
