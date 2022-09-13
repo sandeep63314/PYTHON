6e7059ba6a3399738a22569a714e402a56df7897
Sangram Mohanty <odisha532@hotmail.com>
2022-09-13 11:11:42 +0530

    Changed company name

--- 6_boolean_type.py
+++ 6_boolean_type.py
@@ -1,6 +1,6 @@
 _is_active = True
 # Checks the boolean status of variable. If it is true then if block is executed, if it is false then else block is executed
 if (_is_active):
-    print('You are a TCS employee')
+    print('You are an IBM employee')
 else:
-    print('You cannot enter into TCS')
+    print('You cannot enter into IBM')
