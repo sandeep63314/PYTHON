d51aab52ee5723a8f6d0670a406a0bfa70e94597
Sangram Mohanty <odisha532@hotmail.com>
2022-11-13 15:17:50 +0530

    Modified statement with ternary operator

--- 20_ternary_operator.py
+++ 20_ternary_operator.py
@@ -1,3 +1,8 @@
 # Ternary operator or conditional expression evaluates to a statement if it is true else it evaluates other statement
-_attendance = True
-print('Present' if _attendance else 'Absent')
+
+fsalary = int(input('Enter your father\'s salary:'))
+msalary = int(input('Enter your mother\'s salary:'))
+
+print('Father\'s salary is greater than Mother\'s salary' if fsalary > msalary else 'Mother\'s salary is greater than Father\'s salary')
+
+# salary = fsalary > msalary : 'Father\'s salary is greater than Mother\'s salary';'Mother\'s salary is greater than Father\'s salary'
